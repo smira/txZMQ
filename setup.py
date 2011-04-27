87c2aa0411cf20219377bf1b2052c3aa5b1f5ab5
Andrey Smirnov <smira@qik.com>
2011-04-27 16:40:00 +0400

    Add more lines to README, bump to 0.1.

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
         name='txZMQ',
-        version='0.1dev',
+        version='0.1',
         packages=['txZMQ','txZMQ.test'],
         license='GPLv2',
         author='Andrey Smirnov',
         author_email='me@smira.ru',
         url='http://pypi.python.org/pypi/txZMQ',
-        description='Twisted binding for ZeroMQ',
+        description='Twisted bindings for ZeroMQ',
         long_description=open('README').read(),
         install_requires=["Twisted>=10.0", "pyzmq>=2.1"],
         )
