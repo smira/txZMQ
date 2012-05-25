4da40acba694d7dca3b6947a399fbf304f7d9ed6
Andrey Smirnov <andrey.smirnov@skype.net>
2012-05-25 10:39:57 +0400

    Version bump to 0.5.1.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 
 setup(
         name='txZMQ',
-        version='0.5.0',
+        version='0.5.1',
         packages=['txzmq','txzmq.test'],
         license='GPLv2',
         author='Andrey Smirnov',
