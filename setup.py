6a3d250db6b9ba3c464106f6eeb71ec9577d0370
Andrey Smirnov <Smirnov.Andrey@gmail.com>
2013-10-11 21:35:19 +0400

    Update homepage.

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 
 setup(
-        name='txZMQ',
-        version=open('VERSION').read().strip(),
-        packages=['txzmq', 'txzmq.test'],
-        license='GPLv2',
-        author='Andrey Smirnov',
-        author_email='me@smira.ru',
-        url='http://pypi.python.org/pypi/txZMQ',
-        description='Twisted bindings for ZeroMQ',
-        long_description=open('README.rst').read(),
-        install_requires=["Twisted>=10.0", "pyzmq>=13"],
-        )
+    name='txZMQ',
+    version=open('VERSION').read().strip(),
+    packages=['txzmq', 'txzmq.test'],
+    license='GPLv2',
+    author='Andrey Smirnov',
+    author_email='me@smira.ru',
+    url='https://github.com/smira/txZMQ',
+    description='Twisted bindings for ZeroMQ',
+    long_description=open('README.rst').read(),
+    install_requires=["Twisted>=10.0", "pyzmq>=13"],
+)
